814b39ece18c3ee89688bb837563678742d16628
Edward-Gomez-Q <osval_edward_13@hotmail.com>
2025-05-18 12:54:10 -0400

    Postgress conexion

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
+const common = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
+  development: common,
+  production: common,
 };
